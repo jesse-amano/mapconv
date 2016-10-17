bb544a3dd6064b9aabf402d7f70255093b83b9c0
Jesse Amano <jesse@dealerpeak.com>
2016-10-17 11:15:49 -0700

    Add support for Array, Int*, and Float* types.

--- mapconv.go
+++ mapconv.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"reflect"
 	"strconv"
+	"strings"
 )
 
 // ToMap returns a map[path]value given any arbitrary value.
@@ -20,18 +21,22 @@ func ToMap(value interface{}, prefix string) (m map[string]string, err error) {
 		switch v.Kind() {
 		case reflect.Bool:
 			m[path] = strconv.FormatBool(v.Bool())
-		case reflect.Float64:
-			m[path] = strconv.FormatFloat(v.Float(), 'f', 0, 64)
-		case reflect.Int:
+		case reflect.Float32, reflect.Float64:
+			m[path] = strings.TrimRight(strings.TrimRight(strconv.FormatFloat(v.Float(), 'f', 4, 64), "0"), ".")
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 			m[path] = strconv.FormatInt(v.Int(), 10)
+		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			m[path] = strconv.FormatUint(v.Uint(), 10)
 		case reflect.String:
 			m[path] = v.String()
-		case reflect.Map, reflect.Slice, reflect.Interface:
+		case reflect.Array, reflect.Map, reflect.Slice, reflect.Interface:
 			var subMap map[string]string
 			subMap, err = ToMap(v.Interface(), path)
 			for key, value := range subMap {
 				m[key] = value
 			}
+		case reflect.Invalid, reflect.Chan, reflect.Func, reflect.UnsafePointer, reflect.Complex64, reflect.Complex128:
+			err = fmt.Errorf("mapconv: unsupported values on path (%s) of type (%s)", path, v.Kind())
 		default:
 			err = fmt.Errorf("mapconv: unknown value on path (%s) of type (%s)", path, v.Kind())
 		}
@@ -49,7 +54,7 @@ func ToMap(value interface{}, prefix string) (m map[string]string, err error) {
 				//return
 			}
 		}
-	case reflect.Slice:
+	case reflect.Array, reflect.Slice:
 		for i := 0; i < rv.Len(); i++ {
 			path := prefix + "[" + strconv.Itoa(i+1) + "]"
 			v := rv.Index(i)
